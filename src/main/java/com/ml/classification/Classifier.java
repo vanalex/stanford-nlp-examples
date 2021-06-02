08990ac4971bb6e16fb6f73f22e20af2254c5376
vanalex <apjorquera@gmail.com>
2021-06-02 14:32:31 +0200

    deleting files

--- src/main/java/com/ml/classification/Classifier.java
+++
@@ -1,8 +0,0 @@
-package com.ml.classification;
-
-import weka.classifiers.AbstractClassifier;
-import weka.core.Instances;
-
-public abstract class Classifier<A extends AbstractClassifier> {
-    protected abstract String classify(Instances instances, A a) throws Exception;
-}
--- src/main/java/com/ml/classification/ClassifyArranger.java
+++
@@ -1,8 +0,0 @@
-package com.ml.classification;
-
-import weka.classifiers.AbstractClassifier;
-import weka.core.Instances;
-
-public abstract class ClassifyArranger<A extends AbstractClassifier> {
-    public abstract A arrange(Instances instances) throws Exception;
-}
